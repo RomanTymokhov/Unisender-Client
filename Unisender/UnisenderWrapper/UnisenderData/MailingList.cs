dde5f1d153ffaed04e823857942249fd46ba0dfe
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2018-06-12 20:44:33 +0300

    Добавил в библиотеку слой UnisenderData и реализовал класс с данными о рассылках

---
+++ Unisender/UnisenderWrapper/UnisenderData/MailingList.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnisenderWrapper.UnisenderData
+{
+    //Список рассылок
+    public class MailingList
+    {
+        public List<Mailing> mailingList;
+
+        public MailingList(dynamic pairs)
+        {
+            mailingList = new List<Mailing>();
+            FillList(pairs);
+        }
+
+        private void FillList(dynamic pairs)
+        {
+            foreach (var result in pairs)
+            {
+                foreach (var item in result.Value)
+                {
+                    Mailing mailing = new Mailing(item);
+                    mailingList.Add(mailing);
+                }
+            }
+        }
+    }
+
+    //Объект рассылки
+    public class Mailing
+    {
+        public int mailingId;
+        public string mailingTitle;
+
+        public Mailing(dynamic keyValuePairs)
+        {
+            ParseDictionary(keyValuePairs);
+        }
+
+        private void ParseDictionary(dynamic item)
+        {
+            foreach (var subItem in item)
+            {
+                if (subItem.Key == "id") mailingId = subItem.Value;
+                if (subItem.Key == "title") mailingTitle = subItem.Value;
+            }            
+        }
+    }
+}
