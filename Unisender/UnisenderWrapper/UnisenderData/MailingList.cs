d1d32a50e055a2ddf0c50df6f9a2b54096878dbb
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2018-06-13 14:07:22 +0300

    Добавил в клас Mailing функционал, отвечающий за удаленное создание списка методом createList()

--- Unisender/UnisenderWrapper/UnisenderData/MailingList.cs
+++ Unisender/UnisenderWrapper/UnisenderData/MailingList.cs
@@ -6,6 +6,58 @@ using System.Threading.Tasks;
 
 namespace UnisenderWrapper.UnisenderData
 {
+    //Объект рассылки
+    public class Mailing
+    {
+        public int mailingId;
+        public string mailingTitle;
+
+        public IDictionary<string, object> SendParam { get; private set;}
+
+        private string beforeSubscribeUrl;
+        private string afterSubscribeUrl;
+
+        private const string beforeUrl = "before_subscribe_url";
+        private const string afterUrl = "after_subscribe_url";
+        private const string title = "title";
+        private const string id = "id";
+
+        //конструктор для создания рассылки
+        public Mailing(string mName, string bsu, string asu)
+        {
+            mailingTitle = title;
+            beforeSubscribeUrl = bsu;
+            afterSubscribeUrl = asu;
+
+            CreateSendParam(mName);
+        }
+
+        private void CreateSendParam(string mName)
+        {
+            SendParam = new Dictionary<string, object>();
+
+            SendParam.Add(title, mName);
+            SendParam.Add(beforeUrl, beforeSubscribeUrl);
+            SendParam.Add(afterUrl, afterSubscribeUrl);
+        }
+
+        //конструктор для извлечения рассылки
+        public Mailing(dynamic keyValuePairs)
+        {
+            ParseDictionary(keyValuePairs);
+        }
+
+        private void ParseDictionary(dynamic item)
+        {
+            foreach (var subItem in item)
+            {
+                if (subItem.Key == id) mailingId = subItem.Value;
+                if (subItem.Key == title) mailingTitle = subItem.Value;
+            }            
+        }
+    }
+
+
     //Список рассылок
     public class MailingList
     {
@@ -27,27 +79,6 @@ namespace UnisenderWrapper.UnisenderData
                     mailingList.Add(mailing);
                 }
             }
-        }
-    }
-
-    //Объект рассылки
-    public class Mailing
-    {
-        public int mailingId;
-        public string mailingTitle;
-
-        public Mailing(dynamic keyValuePairs)
-        {
-            ParseDictionary(keyValuePairs);
-        }
-
-        private void ParseDictionary(dynamic item)
-        {
-            foreach (var subItem in item)
-            {
-                if (subItem.Key == "id") mailingId = subItem.Value;
-                if (subItem.Key == "title") mailingTitle = subItem.Value;
-            }            
         }
     }
 }
