9d0e0ca3459acbe1ce4a643c8832ca10532c6543
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2018-06-19 19:15:16 +0300

    Реализовал первую половину метода Subscribe

--- Unisender/UnisenderWrapper/UnisenderMethods/ListMethods.cs
+++ Unisender/UnisenderWrapper/UnisenderMethods/ListMethods.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using UnisenderWrapper.UnisenderData;
+using UnisenderWrapper.UnisenderData.AppData;
 
 namespace UnisenderWrapper.UnisenderMethods
 {
@@ -21,8 +22,6 @@ namespace UnisenderWrapper.UnisenderMethods
 
         public static Mailing CreateList(dynamic client, string listTitle, string bsu, string asu)
         {
-            Mailing mailing = new Mailing(listTitle, bsu, asu);
-
             IDictionary<string, object> sendParam = new Dictionary<string, object>
             {
                 ["title"] = listTitle,
@@ -33,6 +32,9 @@ namespace UnisenderWrapper.UnisenderMethods
             var answer = client.createList(sendParam);
             // todo check error in answer
 
+            //----------------------------------------------------------------------
+            Mailing mailing = new Mailing(listTitle, bsu, asu);
+
             foreach (var result in answer)
             {
                 foreach (var item in result.Value)
@@ -40,7 +42,7 @@ namespace UnisenderWrapper.UnisenderMethods
                     if (item.Key == "id") mailing.Id = item.Value;                  
                 }
             } 
-
+            //----------------------------------------------------------------------
             return mailing;
         }
 
@@ -65,6 +67,33 @@ namespace UnisenderWrapper.UnisenderMethods
             };
 
             var answer = client.deleteList(sendParam);
+        }
+
+
+        public static dynamic Subscribe(dynamic client, List<Mailing>subscribesList, Dictionary<string, object> keyValues, string tags, int dOpt, int oWrite)
+        {
+            /// double_optin 
+            /// 0 - подписчик только высказал желание подписаться, но ещё не подтвердил подписку
+            /// 3 - согласие подписчика уже есть, подписчик добавляется со статусом «новый».
+            /// 4 - система выполняет проверку на наличие подписчика в ваших списках. Если подписчик уже есть в ваших списках со статусом «новый» или «активен»
+            /// owerwrite
+            /// 0 — происходит только добавление новых полей и меток, уже существующие поля сохраняют своё значение
+            /// 1 — все старые поля удаляются и заменяются новыми, все старые метки также удаляются и заменяются новыми
+            /// 2 — заменяются значения переданных полей, если у существующего подписчика есть и другие поля, то они сохраняют своё значение. 
+            /// В случае передачи меток они перезаписываются, если же метки не передаются, то сохраняются старые значения меток
+           
+            IDictionary<string, object> sendParam = new Dictionary<string, object>
+            {
+                ["list_ids"] = subscribesList.ToAdresStr(),
+                ["fields"]   = keyValues,
+                ["tags"] = tags,
+                ["double_optin"] = dOpt,
+                ["overwrite"] = oWrite
+            };
+
+            //TO DO: сделать возвращающий тип Person
+
+            return client.subscribe(sendParam);
         }
     }
 }
