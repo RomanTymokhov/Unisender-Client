ff20a34841c98496cc4cb4c8a7262ba1af1d2d8d
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2018-06-19 22:13:21 +0300

    Реализация системных статус кодов

--- Unisender/UnisenderWrapper/UnisenderData/ServiceData/DoubleOption.cs
+++ Unisender/UnisenderWrapper/UnisenderData/ServiceData/DoubleOption.cs
@@ -6,18 +6,39 @@ using System.Threading.Tasks;
 
 namespace UnisenderWrapper.UnisenderData.ServiceData
 {
-    public static class DoubleOption
+    public class DoubleOption : IServiceStatus
     {
-        private static int zer = 0;
-        private static int one = 1;
-        private static int two = 2;
-        private static int thr = 3;
-        private static int fro = 4;
+        private int zero = 0;
+        private int thre = 3;
+        private int four = 4;
 
-        public static readonly IDictionary<string, object> Zero = new Dictionary<string, object> { ["double_optin"] = zer };
-        public static readonly IDictionary<string, object> One = new Dictionary<string, object> { ["double_optin"] = one };
-        public static readonly IDictionary<string, object> Two = new Dictionary<string, object> { ["double_optin"] = two };
-        public static readonly IDictionary<string, object> Three = new Dictionary<string, object> { ["double_optin"] = thr };
-        public static readonly IDictionary<string, object> Four = new Dictionary<string, object> { ["double_optin"] = fro };
+        public static int Zero
+        {
+            get
+            {
+                DoubleOption doubleOption = new DoubleOption();
+                return doubleOption.zero;
+            }
+        }
+
+        public static int Thre
+        {
+            get
+            {
+                DoubleOption doubleOption = new DoubleOption();
+                return doubleOption.thre;
+            }
+        }
+
+        public static int Four
+        {
+            get
+            {
+                DoubleOption doubleOption = new DoubleOption();
+                return doubleOption.four;
+            }
+        }
+
+        private DoubleOption() { }
     }
 }
---
+++ Unisender/UnisenderWrapper/UnisenderData/ServiceData/Overwrite.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnisenderWrapper.UnisenderData.ServiceData
+{
+    public class Overwrite : IServiceStatus
+    {
+        private int zero = 0;
+        private int one = 1;
+        private int two = 2;
+
+        public static int Zero
+        {
+            get
+            {
+                Overwrite overwrite = new Overwrite();
+                return overwrite.zero;
+            }
+        }
+
+        public static int One
+        {
+            get
+            {
+                Overwrite overwrite = new Overwrite();
+                return overwrite.one;
+            }
+        }
+
+        public static int Two
+        {
+            get
+            {
+                Overwrite overwrite = new Overwrite();
+                return overwrite.two;
+            }
+        }
+
+        private Overwrite() { }
+    }
+}
