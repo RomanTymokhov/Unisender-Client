829b90e6d3d2d8a3234034db10aace9403b358e7
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2018-06-20 22:11:19 +0300

    Классы представляющие объект опций рассылки (для письма подтверждения

---
+++ Unisender/UnisenderWrapper/UnisenderData/MailBody.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnisenderWrapper.UnisenderData
+{
+    public class MailBody
+    {
+        // тут потом реализую данный клас добавив строковые поля по структуре 
+        // самого письма (прехедар и пр. нюансы)
+
+        // возможно данный класс сделаю интерфейсом а от него буду наследовать
+        // различного рода придуманные шаблоны
+
+        // или реализую статич. метод (инстанс) принимающую интерфейс различных шаблонов
+
+        // пока для теста будет только одно строковое поле
+
+        private string mailBody;
+        public static string Get
+        {
+            get
+            {
+                MailBody body = new MailBody();
+                return body.mailBody;
+            }
+        }
+
+        public MailBody ()
+        {
+            mailBody = "<div style = \" padding:75px 15px 15px 50px \">  Щоб наша відповідь була максимально інформативною, пройдіть <a href=\"{{ConfirmUrl}}\" >этой ссылке</a> та залиште відповіді на деякі уточнюючи питання.  </div>";
+        }
+    }
+}
---
+++ Unisender/UnisenderWrapper/UnisenderData/MailingOptions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnisenderWrapper.UnisenderData
+{
+    public partial class MailingOptions
+    {
+        public string SenderName  { get; private set; }
+        public string SenderEmail { get; private set; }
+        public string Subject { get; private set; }
+        public string Body { get; private set; }
+
+        public MailingOptions (string name, string mail, string subject)
+        {
+            SenderName = name;
+            SenderEmail = mail;
+            Subject = subject;
+            Body = MailBody.Get;
+        }
+    }
+}
