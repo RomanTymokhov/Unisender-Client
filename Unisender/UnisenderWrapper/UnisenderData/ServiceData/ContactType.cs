5d209674df08d7c16b42bc7e318ced85f951542e
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2018-06-20 23:13:39 +0300

    Доп классы для реализации метода Unsubscribe

---
+++ Unisender/UnisenderWrapper/UnisenderData/ServiceData/ContactType.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnisenderWrapper.UnisenderData.ServiceData
+{
+    public class ContactType
+    {
+        private string email = "email";
+        private string phone = "phone";
+
+        public static string Email
+        {
+            get
+            {
+                ContactType cType = new ContactType();
+                return cType.email;
+            }
+        }
+
+        public static string Phone
+        {
+            get
+            {
+                ContactType cType = new ContactType();
+                return cType.phone;
+            }
+        }
+    }
+}
---
+++ Unisender/UnisenderWrapper/UnisenderData/ServiceData/UnsubscribeType.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnisenderWrapper.UnisenderData.ServiceData
+{
+    public class UnsubscribeType
+    {
+        private string unsubscribe = "unsubscribe";
+        private string exclude = "exclude";
+
+        public static string Unsubscribe
+        {
+            get
+            {
+                UnsubscribeType unType = new UnsubscribeType();
+                return unType.unsubscribe;
+            }
+        }
+
+        public static string Exclude
+        {
+            get
+            {
+                UnsubscribeType unType = new UnsubscribeType();
+                return unType.exclude;
+            }
+        }
+    }
+}
