033c78168c0c3e9aa4b6454545299448cc9ab695
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2018-06-20 22:13:55 +0300

    Пространство имен по методы расширения Первый класс для расширений класса List (ListExtantion)

---
+++ Unisender/UnisenderWrapper/UnisenderData/AppData/ListExtansion.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnisenderWrapper.UnisenderData.AppData
+{
+    public static class ListExtansion
+    {
+        public static string ToAdresStr(this List<Mailing> list)
+        {
+            string rstr = null;
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                rstr += list.ElementAtOrDefault(i).Id.ToString() + GetComa(i, list.Count);
+            }
+
+            return rstr;
+        }
+
+        private static string GetComa(int itr, int count)
+        {
+            if (itr < count - 1) return ",";
+            else return null;
+        }
+        //--------------------------------------------------------------------------------------------------------//
+
+        public static List<string> ToArr(this List<Mailing> list)
+        {
+            List<string> rstr = new List<string>();
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                rstr.Add(list.ElementAtOrDefault(i).Id.ToString());
+            }
+
+            return rstr;
+        }
+    }
+}
